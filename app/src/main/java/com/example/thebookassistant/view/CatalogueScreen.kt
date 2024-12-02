c244922892fdcec2e97726b13eec0cb6a72c5c42
snznnn <ozensine@gmail.com>
2024-12-02 12:57:16 +0300

    Added keyboard actions to outlinedtextfield

--- app/src/main/java/com/example/thebookassistant/view/CatalogueScreen.kt
+++ app/src/main/java/com/example/thebookassistant/view/CatalogueScreen.kt
@@ -1,7 +1,9 @@
 package com.example.thebookassistant.view
 
 import android.content.Context
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -29,7 +33,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.thebookassistant.api.RetrofitInstance
@@ -59,14 +65,22 @@ fun CatalogueScreen(navController: NavHostController) {
 
         var errorMessage by remember { mutableStateOf<String?>(null) }
         var noResultsFound by remember { mutableStateOf(false) }
-// @formatter:off
-        Column(modifier = Modifier.fillMaxSize().padding(padding), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
-// @formatter:on
-            OutlinedTextField(value = title, onValueChange = { title = it }, label = { Text("Title") }, modifier = Modifier.fillMaxWidth(0.8f))
-            Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(value = authorName, onValueChange = { authorName = it }, label = { Text("Author") }, modifier = Modifier.fillMaxWidth(0.8f))
-            Spacer(modifier = Modifier.height(8.dp))
 
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+
+        Box(
+            modifier = Modifier.fillMaxSize().padding(padding).pointerInput(Unit) {
+                    detectTapGestures(onTap = { keyboardController?.hide() })}){
+
+        }
+        Column(modifier = Modifier.fillMaxSize().padding(padding), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
+          OutlinedTextField(value = title, onValueChange = { title = it }, label = { Text("Title") }, modifier = Modifier.fillMaxWidth(0.8f),
+                keyboardOptions = KeyboardOptions.Default, keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }))
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(value = authorName, onValueChange = { authorName = it }, label = { Text("Author") }, modifier = Modifier.fillMaxWidth(0.8f),
+                keyboardOptions = KeyboardOptions.Default, keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }))
+            Spacer(modifier = Modifier.height(8.dp))
             Row(horizontalArrangement = Arrangement.spacedBy(16.dp), verticalAlignment = Alignment.CenterVertically) {
                 Button(
                     onClick = {
@@ -144,14 +158,12 @@ private fun CatalogueScreenList(books: List<SearchApiResponseDoc>, context: Cont
 
     LazyColumn(modifier = Modifier.fillMaxWidth(0.8f)) {
         items(books) { book ->
-// @formatter:off
             Card(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp), elevation = CardDefaults.cardElevation(4.dp)) {
                 Row(modifier = Modifier.fillMaxWidth().padding(8.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
                     Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center) {
                         Text(text = "Title: ${book.title}", style = MaterialTheme.typography.bodyMedium)
                         Text(text = "Author(s): ${book.author_name.joinToString(", ")}", style = MaterialTheme.typography.bodySmall)
                     }
-// @formatter:on
                     Button(
                         onClick = {
                             val favoriteBook = FavoritedBooks(title = book.title, authors = book.author_name.joinToString(", "))
