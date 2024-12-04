6b3a6b9f42dd33f594a8a32d68653b9789ef6d5c
snznnn <ozensine@gmail.com>
2024-12-04 19:20:49 +0300

    enabled okhttp cache

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,11 +59,14 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
+
     implementation(libs.room.runtime)
     kapt(libs.room.compiler)
     implementation(libs.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/thebookassistant/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/thebookassistant/api/RetrofitInstance.kt
@@ -1,7 +1,10 @@
 package com.example.thebookassistant.api
 
+import android.content.Context
 import com.example.thebookassistant.api.chatgpt.ChatGptCompletionsApiService
 import com.example.thebookassistant.api.openlibrary.OpenLibrarySearchApiService
+import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,18 +15,29 @@ object RetrofitInstance {
     private const val OPEN_LIBRARY_BASE_URL = "https://openlibrary.org/"
     private const val CHAT_GPT_BASE_URL = "https://api.openai.com/v1/chat/"
 
-    val openLibrarySearchApiService: OpenLibrarySearchApiService by lazy {
-        Retrofit.Builder().baseUrl(OPEN_LIBRARY_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()).build()
-            .create(OpenLibrarySearchApiService::class.java)
+    private fun provideOkHttpClient(context: Context): OkHttpClient {
+        val cacheSize = 10L * 1024 * 1024 // 10 mb
+        val cache = Cache(context.cacheDir, cacheSize)
+
+        val networkCacheInterceptor = Interceptor { chain ->
+            val response = chain.proceed(chain.request())
+            response.newBuilder().header("Cache-Control", "public, max-age=60") // 60 sec
+                .removeHeader("Pragma").build()
+        }
+
+        return OkHttpClient.Builder().cache(cache).addNetworkInterceptor(networkCacheInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS).build()
     }
 
-    val chatGptCompletionsApiService: ChatGptCompletionsApiService by lazy {
-        val okHttpClient = OkHttpClient.Builder().connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).build()
+    fun provideOpenLibrarySearchApiService(context: Context): OpenLibrarySearchApiService {
+        return Retrofit.Builder().baseUrl(OPEN_LIBRARY_BASE_URL)
+            .client(provideOkHttpClient(context)).addConverterFactory(GsonConverterFactory.create())
+            .build().create(OpenLibrarySearchApiService::class.java)
+    }
 
-        Retrofit.Builder().baseUrl(CHAT_GPT_BASE_URL)
-            .client(okHttpClient)
+    fun provideChatGptCompletionsApiService(context: Context): ChatGptCompletionsApiService {
+        return Retrofit.Builder().baseUrl(CHAT_GPT_BASE_URL).client(provideOkHttpClient(context))
             .addConverterFactory(GsonConverterFactory.create()).build()
             .create(ChatGptCompletionsApiService::class.java)
     }
